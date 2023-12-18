2a0e855b20184425a97c5ade10f054399a22cf25
Cavin <cavinlarry001@gmail.com>
2023-12-18 14:00:49 +0300

    Added new files

---
+++ control-flow/array.js
@@ -0,0 +1,12 @@
+let numbers = [21, 45, 100, 12, 11, 78, 61, 4, 39, 22];
+console.log(numbers);
+
+for( number of numbers){
+	if (number % 2 == 0){
+		console.log(`${number} is even`);
+	}
+
+	else{
+		console.log(`${number} is odd`);
+	}
+}
---
+++ control-flow/while.js
@@ -0,0 +1,5 @@
+let n = 0;
+while (n <= 101){
+	console.log(`${n}`);
+	n += 10;
+}
