4b6289cb7dc1651d1943fcc0641ae3d61dc66b85
Cavin <cavinlarry001@gmail.com>
2024-04-23 15:03:58 +0300

    Added new files

---
+++ beginner-javascript1/array/array.js
@@ -0,0 +1,7 @@
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+
+let arrLength = myArray.length;
+console.log(arrLength);
+
+console.log(Math.max(1, 2, 3, 4, 5));
---
+++ beginner-javascript1/basic-js/binding.js
@@ -0,0 +1,5 @@
+let mood = 'light';
+console.log("Initial mood is",mood);
+
+mood = 'dark' // mood re declared as dark
+console.log("Current mood is",mood);
